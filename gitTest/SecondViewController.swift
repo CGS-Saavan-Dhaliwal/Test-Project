b633925ddf81e5dd0307763b9cd8ae131273757c
Saavan Dhaliwal <16569@cgs.act.edu.au>
2017-06-19 10:36:07 +1000

    Updated-Second-View-Controller

---
+++ gitTest/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController2.swift
+//  gitTest
+//
+//  Created by Saavan Dhaliwal on 19/6/17.
+//  Copyright © 2017 Saavan Dhaliwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("debugMyCode")
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//ViewController2
+}
